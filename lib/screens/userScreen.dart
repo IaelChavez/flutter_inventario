78be3c4a4a5ab0d8e536d5b2ea5cfb8af02bb105
Jesus072466 <jco72466@udelasalle.edu.mx>
2023-04-20 16:20:07 -0600

    Vista usuario

--- lib/screens/userScreen.dart
+++ lib/screens/userScreen.dart
@@ -1,13 +1,111 @@
 import 'package:flutter/material.dart';
 
-class userScreen extends StatelessWidget {
-  const userScreen({Key? key}) : super(key: key);
+class userView extends StatefulWidget {
+  @override
+  _userView createState() => _userView();
+}
+
+class _userView extends State<userView> {
+  _userView({Key? key}) : super();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('userScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Formulario'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Text('Nombre       '),
+                  Expanded(
+                    child: TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, ingrese su nombre';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Text('Correo          '),
+                  Expanded(
+                    child: TextFormField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, ingrese su correo electrónico';
+                        }
+                        if (!value.contains('@')) {
+                          return 'Ingrese un correo electrónico válido';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Text('Contraseña   '),
+                  Expanded(
+                    child: TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, ingrese su contraseña';
+                        }
+                        if (value.length < 6) {
+                          return 'La contraseña debe tener al menos 6 caracteres';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Aquí va la lógica para guardar los datos del formulario
+                  }
+                },
+                child: Text('Guardar'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
